9aae1272b6587bae7623263ec9d5e3cd56a39aea
Petr Šebela <mail@sebelapetr.cz>
2023-02-26 02:11:02 +0100

    remove asset files

--- cmd.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-exit(App\Bootstrap::boot()
-	->createContainer()
-	->getByType(\Contributte\Console\Application::class)
-	->run());
